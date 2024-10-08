92b9701ada41d5c27f0aaf4ff11c0ab37fe6694c
yun <ly.yun811@gmail.com>
2024-10-08 14:32:22 -0700

    review js css html by making a mini player page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let lists = ["1h", "2e", "3l", "4lo"];
-  let listsHTML = lists.map((list) => {
-    return (<li>{list}</li>)
-  });
+  let commentsData = [
+    { creator: "A", comments: "YAY", likedCount: 1 },
+    { creator: "B", comments: "GOOD", likedCount: 10 },
+    { creator: "C", comments: "NICE", likedCount: 2 },
+  ]
   return (
-    <ul className='listsHTML'>
-      {listsHTML}
-    </ul>
+  <div className='App'>
+    <div className='columns'>
+      <div className='primary'>
+        <div className='video-player'>Video-Player</div>
+        <div className='comments'>
+          {commentsData.map((comment)=>{
+            return (
+              <div className='comment'>
+                <h3 classname='comment-header'>{ comment.creator }</h3>
+                <p className='comment-body'>{ comment.comment }</p>
+                <div className='comment-actions'>
+                  <button>Like</button>
+                  <span> {comment.likedCount }</span>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className='secondary'>
+          <div className='recommendations'>Recommendations</div>
+      </div>
+    </div>
+  </div>
   );
 }
 
